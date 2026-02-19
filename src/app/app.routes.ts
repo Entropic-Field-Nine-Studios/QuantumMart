97d0c5019a74e84b88fe04815ea42298fe06fd6b
Matthew Cumbo <mattcumbo99@gmail.com>
2026-02-18 18:06:33 -0600

    Guards for login and registration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { RegistrationComponent } from './registration/registration';
 import { HomeComponent } from './home/home';
 import { AboutUs } from './about-us/about-us';
 import { Support } from './support/support';
+import { userRedirectGuard } from './auth/user-redirect-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about-us', component: AboutUs },
   { path: 'contact', component: Support },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent, canActivate: [userRedirectGuard] },
+  { path: 'register', component: RegistrationComponent, canActivate: [userRedirectGuard] },
 ];
