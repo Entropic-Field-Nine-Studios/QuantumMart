898090c28d7156ce3e944e62730a78528fcb072f
MattCumbo99 <mattcumbo99@users.noreply.github.com>
2026-03-09 17:15:59 -0500

    Prettier fix

--- src/app/create-item-listing/create-item-listing.guard.ts
+++ src/app/create-item-listing/create-item-listing.guard.ts
@@ -2,7 +2,7 @@ import { CanDeactivateFn } from '@angular/router';
 import { CreateItemListingComponent } from './create-item-listing.component';
 
 export const createItemListingGuard: CanDeactivateFn<CreateItemListingComponent> = (
-  component: CreateItemListingComponent
+  component: CreateItemListingComponent,
 ) => {
   if (component.canDeactivate()) {
     return true;
