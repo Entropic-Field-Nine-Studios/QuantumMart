6eb6ea1f34a17a68013a9b5296732cd7991662cd
MattCumbo99 <mattcumbo99@users.noreply.github.com>
2026-03-12 16:02:52 -0500

    Message service and bug fixes

--- src/app/shared/message/message.service.ts
+++ src/app/shared/message/message.service.ts
@@ -1,38 +1,65 @@
 import { inject, Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-const DEFAULT_DURATION_MS = 15000;
-
+/**
+ * A service for displaying snackbar-like messages on the screen.
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class MessageService {
   private readonly snackbar = inject(MatSnackBar);
 
+  /**
+   * Displays a green snackbar message at the bottom of the screen.
+   *
+   * @param message Text to display.
+   */
   success(message: string) {
     this.snackbar.open(message, 'OK', {
-      duration: DEFAULT_DURATION_MS,
+      duration: this.determineDuration(message),
       panelClass: ['snackbar-success'],
-      horizontalPosition: 'center',
+      horizontalPosition: 'left',
       verticalPosition: 'bottom',
     });
   }
 
+  /**
+   * Displays a message as an error at the top of the screen. Automatically appends 'Error:' if
+   * the message provided doesn't have it as a prefix.
+   *
+   * @param message Text to display.
+   */
   error(message: string) {
-    this.snackbar.open(message, 'Dismiss', {
-      duration: DEFAULT_DURATION_MS,
+    const modifiedMsg = message.startsWith('Error') ? message : `Error: ${message}`;
+
+    this.snackbar.open(modifiedMsg, 'Dismiss', {
+      duration: this.determineDuration(message),
       panelClass: ['snackbar-error'],
       horizontalPosition: 'center',
-      verticalPosition: 'bottom',
+      verticalPosition: 'top',
     });
   }
 
+  /**
+   * Displays a normal snackbar message at the bottom of the screen.
+   *
+   * @param message Text to display.
+   */
   info(message: string) {
     this.snackbar.open(message, 'OK', {
-      duration: DEFAULT_DURATION_MS,
+      duration: this.determineDuration(message),
       panelClass: ['snackbar-info'],
       horizontalPosition: 'center',
       verticalPosition: 'bottom',
     });
+  }
+
+  private determineDuration(message: string): number {
+    const base = 5000; // Minimum duration
+    const perChar = 40; // Increment per character
+    const max = 20000; // Maximum time allowed
+
+    return Math.min(base + message.length * perChar, max);
   }
 }
