94476b02ac333fa2e78cc2a56d8e44e15564c3aa
BobDoland <46633981+BobDoland@users.noreply.github.com>
2026-03-23 16:10:10 -0500

    Made item listing page

--- src/app/item-listings/item-listing-page/item-listing-page.component.ts
+++ src/app/item-listings/item-listing-page/item-listing-page.component.ts
@@ -1,53 +1,102 @@
-import { Component, inject, Input, OnChanges, OnInit } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ItemListing } from '../item-listing.model';
-import { MatFabButton } from '@angular/material/button';
-import { CartItemService } from '../../cart/cart-item.service';
-import { AuthService } from '../../auth/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DateService } from '../../date/date.service';
+import { User } from '../../users/user.model';
+import { UserService } from '../../users/user.service';
 import { MessageService } from '../../shared/message/message.service';
+import { ItemListingService } from '../item-listing.service';
 import { HttpErrorResponse } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatCard, MatCardTitle, MatCardHeader, MatCardAvatar } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ItemListingListComponent } from '../item-listing-list/item-listing-list.component';
+import { CurrencyPipe } from '@angular/common';
+import { AuthService } from '../../auth/auth.service';
+import { CartItemService } from '../../cart/cart-item.service';
 import { MatIcon } from '@angular/material/icon';
-import { MatTooltip } from '@angular/material/tooltip';
+import { MatFabButton } from '@angular/material/button';
 
 @Component({
-  selector: 'app-item-listing',
-  imports: [MatCardModule, CurrencyPipe, MatIcon, MatTooltip, MatFabButton],
+  imports: [
+    MatCard,
+    MatCardTitle,
+    MatCardHeader,
+    MatCardAvatar,
+    MatGridListModule,
+    MatProgressSpinner,
+    ItemListingListComponent,
+    CurrencyPipe,
+    MatIcon,
+    MatFabButton,
+  ],
   templateUrl: './item-listing-page.component.html',
   styleUrl: './item-listing-page.component.scss',
 })
-export class ItemListingPageComponent implements OnInit, OnChanges {
-  @Input({ required: true }) listing!: ItemListing;
-  @Input() showUsername = true;
-
-  loggedIn = false;
+export class ItemListingPageComponent implements OnInit {
+  private userService = inject(UserService);
+  private itemListingService = inject(ItemListingService);
+  private dateService = inject(DateService);
+  private activatedRoute = inject(ActivatedRoute);
+  private router = inject(Router);
+  private messageService = inject(MessageService);
+  private authService = inject(AuthService);
+  private cartService = inject(CartItemService);
+  user = signal<User | null>(null);
+  sDateCreated!: string;
+  listing = signal<ItemListing | null>(null);
   isListingCurrentUser = false;
-
-  private readonly cartService = inject(CartItemService);
-  private readonly authService = inject(AuthService);
-  private readonly messageService = inject(MessageService);
-  private readonly router = inject(Router);
+  loggedIn = false;
 
   ngOnInit(): void {
-    this.loggedIn = this.authService.isLoggedIn;
-    this.isListingCurrentUser = this.authService.userId === this.listing.sellerId;
+    this.itemListingService
+      .getListingById(this.activatedRoute.snapshot.params['listid'])
+      .subscribe({
+        next: (listing1) => {
+          this.listing.set(listing1);
+          this.isListingCurrentUser = this.authService.userId === listing1.sellerId;
+          this.loggedIn = this.authService.isLoggedIn;
+          this.userService.getUserById(listing1.sellerId ?? '').subscribe({
+            next: (data) => this.user.set(data),
+          });
+        },
+        error: (err: HttpErrorResponse) => {
+          switch (err.status) {
+            case 400:
+            case 404:
+              this.router.navigate(['/404']);
+              break;
+            default:
+              this.messageService.error(err.message);
+          }
+        },
+      });
   }
 
   ngOnChanges(): void {
     this.loggedIn = this.authService.isLoggedIn;
-    this.isListingCurrentUser = this.authService.userId === this.listing.sellerId;
-  }
-
-  navigateToUser() {
-    this.router.navigate([`/profile/${this.listing.sellerId}`]);
+    this.isListingCurrentUser = this.authService.userId === this.listing()!.sellerId;
   }
 
   addToCart(): void {
-    this.cartService.addItemToCart(this.authService.userId!, this.listing).subscribe({
+    this.cartService.addItemToCart(this.authService.userId!, this.listing()!).subscribe({
       next: (item) => this.messageService.info(`${item.itemListing.title} was added to your cart.`),
       error: (err: HttpErrorResponse) =>
         this.messageService.error(`Couldn't add item: ${err.message}`),
     });
+  }
+
+  openAddListingDialog(): void {
+    this.router.navigate(['/create-listing']);
+  }
+
+  openCartDialog(): void {
+    this.router.navigate(['/cart']);
+  }
+
+  navigateToUser() {
+    this.router.navigate([`/profile/${this.listing()!.sellerId}`]);
   }
 }
