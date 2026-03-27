6d4e010768ebcdb9ef7536379ba017aa5e248a37
MattCumbo99 <mattcumbo99@users.noreply.github.com>
2026-03-27 15:41:47 -0500

    Separated top header

---
+++ src/app/top-header/top-header.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject } from '@angular/core';
+import { Router, RouterLinkWithHref } from '@angular/router';
+import { User } from '../users/user.model';
+import { UserStore } from '../core/stores/user.store';
+import { NotFoundComponent } from '../not-found/not-found.component';
+import { UserCardComponent } from '../user-card/user-card.component';
+import { MatTabNav, MatTabNavPanel, MatTabsModule } from '@angular/material/tabs';
+
+@Component({
+  selector: 'app-top-header',
+  imports: [RouterLinkWithHref, UserCardComponent, MatTabNavPanel, MatTabNav, MatTabsModule],
+  templateUrl: './top-header.component.html',
+  styleUrl: './top-header.component.scss',
+})
+export class TopHeaderComponent {
+  private readonly loginRegisterRoutes = ['/login', '/register'];
+
+  private readonly router = inject(Router);
+  private readonly userStore = inject(UserStore);
+
+  get user(): User | null {
+    return this.userStore.user();
+  }
+
+  showUserCard(): boolean {
+    return (
+      !this.loginRegisterRoutes.includes(this.router.url) && !this.isAtCheckout() && !this.isAt404()
+    );
+  }
+
+  showNavTabs(): boolean {
+    return !this.isAtCheckout() && !this.isAt404() && !this.isAtSettings();
+  }
+
+  private isAtCheckout(): boolean {
+    return this.router.url.includes('/checkout');
+  }
+
+  private isAt404(): boolean {
+    return this.router.routerState.snapshot.root.firstChild?.component === NotFoundComponent;
+  }
+
+  private isAtSettings(): boolean {
+    return this.router.url.includes('/account-settings');
+  }
+}
