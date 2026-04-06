e44a2b463ced7fd1bae0a7a54a31220d0f546584
MattCumbo99 <mattcumbo99@users.noreply.github.com>
2026-04-06 15:26:59 -0500

    Test fix

--- src/app/top-header/top-header.component.spec.ts
+++ src/app/top-header/top-header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TopHeaderComponent } from './top-header.component';
+import { provideRouter } from '@angular/router';
 
 describe('TopHeaderComponent', () => {
   let component: TopHeaderComponent;
@@ -9,6 +10,7 @@ describe('TopHeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TopHeaderComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TopHeaderComponent);
