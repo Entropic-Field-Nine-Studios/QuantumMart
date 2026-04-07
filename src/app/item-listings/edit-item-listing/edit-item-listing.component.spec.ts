d2f471f335c37d646f4be82a500300c01396405a
MattCumbo99 <mattcumbo99@users.noreply.github.com>
2026-04-07 17:53:05 -0500

    Route guard and page

---
+++ src/app/item-listings/edit-item-listing/edit-item-listing.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditItemListingComponent } from './edit-item-listing.component';
+
+describe('EditItemListingComponent', () => {
+  let component: EditItemListingComponent;
+  let fixture: ComponentFixture<EditItemListingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [EditItemListingComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EditItemListingComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/item-listings/edit-item-listing/edit-item-listing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-item-listing',
+  imports: [],
+  templateUrl: './edit-item-listing.component.html',
+  styleUrl: './edit-item-listing.component.scss',
+})
+export class EditItemListingComponent {
+
+}
